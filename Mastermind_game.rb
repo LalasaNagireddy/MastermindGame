463a85bdf7cfc1c95f34c89d60a2cc982bffea76
LalasaNagireddy <157078820+LalasaNagireddy@users.noreply.github.com>
2024-03-31 21:28:02 -0400

    Update Mastermind_game.rb

--- Mastermind_game.rb
+++ Mastermind_game.rb
@@ -1,7 +1,7 @@
 # Code class represents the secret code to be guessed
 class Code
   attr_reader :sequence
-  
+
   def initialize(sequence)
     @sequence = sequence
   end
@@ -10,7 +10,7 @@ end
 # Player class represents the player of the game
 class Player
   attr_reader :name
-  
+
   def initialize(name)
     @name = name
   end
@@ -18,29 +18,35 @@ end
 
 # Game class represents the Mastermind game
 class Game
-  attr_reader :player, :secret_code, :attempts_left, :code_length
-  
-  def initialize(player, code_length)
+  attr_reader :player, :secret_code, :attempts_left, :code_length, :allow_duplicates
+
+  def initialize(player, code_length, allow_duplicates)
     @player = player
     @code_length = code_length
+    @allow_duplicates = allow_duplicates
     @secret_code = Code.new(generate_secret_code)
     @attempts_left = 10
   end
-  
+
   def generate_secret_code
     colors = ['Red', 'Green', 'Blue', 'Yellow', 'Purple', 'Orange', 'White', 'Silver']
-    Array.new(@code_length) { colors.sample }
+    code = []
+    while code.length < @code_length
+      color = colors.sample
+      code << color if @allow_duplicates || !code.include?(color)
+    end
+    code
   end
-  
+
   def make_guess(guess)
     @attempts_left -= 1
     compare_codes(guess)
   end
-  
+
   def compare_codes(guess)
     black_pegs = 0
     white_pegs = 0
-    
+
     guess.each_with_index do |color, index|
       if color == @secret_code.sequence[index]
         black_pegs += 1
@@ -48,10 +54,10 @@ class Game
         white_pegs += 1
       end
     end
-    
+
     [black_pegs, white_pegs]
   end
-  
+
   def game_over?
     @attempts_left <= 0
   end
@@ -70,23 +76,25 @@ until [4, 6, 8].include?(code_length)
   code_length = gets.chomp.to_i
 end
 
-game = Game.new(player, code_length)
+puts "Allow duplicates in the code? (yes/no):"
+allow_duplicates = gets.chomp.downcase == 'yes'
+
+game = Game.new(player, code_length, allow_duplicates)
 
 puts "Available colors: Red, Green, Blue, Yellow, Purple, Orange, White, Silver"
+puts "Make a guess by typing a sequence of colors separated by spaces."
+puts "For example, for a code length of #{code_length}, you could guess:"
+example_colors = ['Red', 'Green', 'Blue', 'Yellow', 'Purple', 'Orange', 'White', 'Silver']
+puts example_colors.take(code_length).join(' ')
 
 until game.game_over?
   puts "Attempts left: #{game.attempts_left}"
-  puts "Make a guess by typing a sequence of colors separated by spaces."
-  puts "For example, for a code length of #{code_length}, you could guess:"
-  puts "Red Green Blue Yellow" if code_length == 4
-  puts "Red Green Blue Yellow Purple Orange" if code_length == 6
-  puts "Red Green Blue Yellow Purple Orange White Silver" if code_length == 8
   guess = gets.chomp.split.map(&:capitalize)
   feedback = game.make_guess(guess)
   puts "Feedback: #{feedback[0]} black peg(s) and #{feedback[1]} white peg(s)"
 end
 
-if ((feedback[0] == 4) or (feedback[0] == 6) or (feedback[0] == 8))
+if game.secret_code.sequence == guess
   puts "Congratulations, #{player_name}! You guessed the secret code #{game.secret_code.sequence.join(' ')}!"
 else
   puts "Game over. You ran out of attempts. The secret code was #{game.secret_code.sequence.join(' ')}."
